36a3bba4d3a10b2e3056ea2b62e0b73a3f71f467
Michael Ojo <74685782+MY-KHEL@users.noreply.github.com>
2024-02-18 20:45:24 +0000

    portfolio update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,61 +1,140 @@
 import { project_data } from "../data/projects_data";
 import { motion } from "framer-motion";
 
-const projectAnimation ={
-    initial:{
-        x:-200,
-        opacity:0,
-        
+const projectAnimation = {
+  initial: {
+    x: -200,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.7,
     },
-    animate:{
-       
-            x:0,
-            opacity:1,
-            transition:{
-                delay:0.7,
-            }
-    }
-}
+  },
+};
 
 const Projects = () => {
-    return ( 
+  return (
     <>
-    <h2 className="font-bold text-4xl md:text-4xl mt-4 underline text-center " id="projects">PROJECTS</h2>
+      <h2
+        className="font-bold text-4xl md:text-4xl mt-4 font-text text-center "
+        id="projects"
+      >
+        PROJECTS
+      </h2>
 
-    <div className=" px-4 md:px-8 mt-8 md:mt-16  ">
-       {project_data.map((item)=>{
-            return(
-                <div className="grid md:grid-cols-2 px-4 mb-8 " key={item.id}>
-                <motion.div className="md:col-span-1"
+      <div className=" px-4 md:px-8 mt-8 md:mt-16  ">
+        {project_data.map((item) => {
+          return (
+            <div
+              className="hidden md:grid md:grid-cols-2 px-4 mb-8 "
+              key={item.id}
+            >
+              <motion.div
+                className="md:col-span-1"
                 variants={projectAnimation}
-                initial='initial'
-                animate = 'animate'
-                >
-                 <h2 className="font-semibold text-2xl mb-2 "> {item.title} </h2>
-                 <p> {item.content}</p>
-                 {item.hasDemo &&
-                 <span>
+                initial="initial"
+                animate="animate"
+              >
+                <h2 className="font-semibold text-2xl mb-2 "> {item.title} </h2>
+                <p> {item.content}</p>
+                {item.hasDemo && (
+                  <span>
                     <a href={item.link1} target="_blank" rel="noreferrer">
-                 <button className="bg-secColor dark:bg-gradient-to-l from-purple to-pink py-2 px-6 text-white rounded">Demo 
-                
-                     </button></a>
-                     <a href={item.link2} target="_blank" rel="noreferrer">
-                 <button className="bg-secColor dark:bg-gradient-to-r from-purple to-pink mx-2 md:mx-4 mt-3 text-white  py-2 px-6 rounded">Github </button></a>
-                 </span> }
-                </motion.div>
-                {/* <div className=" hidden md:grid md:col-span-1 "></div> */}
-                < motion.div
-                whileHover={{scale:1.1}}
-                 className="text-center mt-4 md:mt-0 md:mx-16">
-                    <img src={item.source} alt="project"  className="w-full shadow-md rounded-md" />
-                </motion.div>
+                      <button className="bg-green dark:bg-gradient-to-l from-purple to-pink py-2 px-6 text-white rounded">
+                        Demo &nbsp;{" "}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6 inline"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+                          />
+                        </svg>
+                      </button>
+                    </a>
+                    <a href={item.link2} target="_blank" rel="noreferrer">
+                      <button className="bg-secColor dark:bg-gradient-to-r from-purple to-pink mx-2 md:mx-4 mt-3 text-white  py-2 px-6 rounded">
+                        Github{" "}
+                      </button>
+                    </a>
+                  </span>
+                )}
+              </motion.div>
+              {/* <div className=" hidden md:grid md:col-span-1 "></div> */}
+              <motion.div
+                whileHover={{ scale: 1.1 }}
+                className="text-center mt-4 md:mt-0 md:mx-16"
+              >
+                <img
+                  src={item.source}
+                  alt="project"
+                  className="w-full shadow-md rounded-md"
+                />
+              </motion.div>
             </div>
-            )
-       }) }
-        <hr/>
-    </div>
+          );
+        })}
+        <hr />
+      </div>
+      {/* Mobile View */}
+      <div className="md:hidden grid grid-cols-1 md:grid-cols-4 gap-4 px-4 md:px-8 mt-8 mb-6 ">
+        {project_data.map((item) => {
+          return (
+            <div key={item.id} className="shadow-lg rounded-md  text-left ">
+              <img
+                src={item.source}
+                alt="project"
+                className="w-full  rounded-t-md"
+              />
+              <h2 className="font-semibold text-2xl mb-2 p-2 ">
+                {" "}
+                {item.title}{" "}
+              </h2>
+              <p className="font-normal text-left p-2">{item.content} </p>
+              {item.hasDemo && (
+                <span className="px-2">
+                  <a href={item.link1} target="_blank" rel="noreferrer">
+                    <button className="bg-green dark:bg-gradient-to-l from-purple to-pink py-2 px-6 text-white rounded">
+                      Demo &nbsp;
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5 inline"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+                        />
+                      </svg>
+                    </button>
+                  </a>
+                  <a href={item.link2} target="_blank" rel="noreferrer">
+                    <button className="bg-secColor dark:bg-gradient-to-r from-purple to-pink mx-2 md:mx-4 mt-3 text-white  py-2 px-6 rounded">
+                      Github{" "}
+                    </button>
+                  </a>
+                </span>
+              )}
+              <p className="mt-4">{item.icon}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
-     );
-}
+  );
+};
 
 export default Projects;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  darkMode:'class',
+  darkMode: "class",
   theme: {
     colors: {
-      'bgColor': '#E1E6E1',
-      'secColor': '#232528',
-      'white':"#ffffff",
-      "black":"#020617",
-      "pink":"#db2777",
-      "purple":"#312e81",
-      "one":"#CACFF9",
-      "two":"#F5CBD9",
-      "three":"#F4BCCD",
-  
+      bgColor: "#FEFEFA",
+      secColor: "#232528",
+      white: "#ffffff",
+      black: "#020617",
+      pink: "#db2777",
+      purple: "#312e81",
+      green: "#15803d",
+      one: "#CACFF9",
+      two: "#F5CBD9",
+      three: "#F4BCCD",
     },
     extend: {},
     borderRadius: {
-      'none': '0',
-      'sm': '0.125rem',
-      DEFAULT: '0.25rem',
-      
-      'md': '0.375rem',
-      'lg': '0.5rem',
-      'full': '9999px',
-      'large': '12px',
-    }
+      none: "0",
+      sm: "0.125rem",
+      DEFAULT: "0.25rem",
+
+      md: "0.375rem",
+      lg: "0.5rem",
+      full: "9999px",
+      large: "12px",
+    },
   },
   plugins: [],
-}
-
+};
