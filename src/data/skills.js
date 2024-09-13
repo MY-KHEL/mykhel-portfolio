dbed5eec5339778e3be3e11a72a0d9160ea96c85
Michael Ojo <74685782+MY-KHEL@users.noreply.github.com>
2024-09-13 00:57:15 +0000

    ghj

---
+++ src/data/skills.js
@@ -0,0 +1,38 @@
+export const skills = [
+  {
+    id: 1,
+    skill: "Javascript",
+  },
+  {
+    id: 2,
+    skill: "Tailwind CSS",
+  },
+  {
+    id: 3,
+    skill: "HTML",
+  },
+  {
+    id: 4,
+    skill: "React Js",
+  },
+  {
+    id: 5,
+    skill: "REST APIs",
+  },
+  {
+    id: 6,
+    skill: "Framer Motion",
+  },
+  {
+    id: 7,
+    skill: "Github",
+  },
+  {
+    id: 8,
+    skill: "Responsive Design",
+  },
+  {
+    id: 9,
+    skill: "Firebase",
+  },
+];
